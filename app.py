ecbfe6baaa12d7634a31c67ae86fbf411c9a64cb
tehvova787 <tehgsl260@gmail.com>
2025-05-08 06:13:58 +0400

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -126,4 +126,5 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True) 
